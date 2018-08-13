f13a6051af63c6eb0660418deeb2e3e7909196b1
Vinokurov Kirill <vinokurov@elewise.com>
2018-08-13 10:31:27 +0400

    feature/#24

--- app/src/main/java/my/neomer/tapper/MapActor.java
+++ app/src/main/java/my/neomer/tapper/MapActor.java
@@ -1,14 +1,19 @@
 package my.neomer.tapper;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
 public class MapActor extends BaseActor {
+
+    Coordinate mMapSizes;
+    Byte [][] mMap;
 
     MapActor(Coordinate position, Sprite sprite, Material material) {
         super(position, sprite, material);
 
-    }
-
-    void CreateActors() {
-        byte [][] map = {
+        mMapSizes = new Coordinate(42, 16);
+        mMap = new Byte [][] {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -26,6 +31,42 @@ public class MapActor extends BaseActor {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
+
+    }
+
+    void CreateActors() {
+    }
+
+    @Override
+    public void Draw(Canvas canvas) {
+        Coordinate coords = GetCoordinates().Clone();
+        int screenWidth = 10;
+        int dx = (int)(canvas.getWidth() / screenWidth);
+        int dy = (int)(canvas.getHeight() / mMapSizes.getY());
+
+        Paint spacePaint = new Paint();
+        spacePaint.setColor(Color.parseColor("#000000"));
+
+        Paint brickPaint = new Paint();
+        brickPaint.setColor(Color.parseColor("#ff0000"));
+
+        canvas.drawColor(Color.BLACK);
+
+        for (int x = 0; x < screenWidth; x++)
+        {
+            for (int y = 0; y < mMapSizes.getY(); y++)
+            {
+                switch (mMap[x][y])
+                {
+                    case 1:
+                        canvas.drawRect();
+                        break;
+
+                    default:
+                        continue;
+                }
+            }
+        }
     }
 
     @Override
